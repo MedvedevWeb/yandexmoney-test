32afecc2e5e7afb09d4b9257452f0f8cebb276c2
Medvedev Nikita <medvedev.web.dev@gmail.com>
2017-09-01 01:28:39 +0300

    Test form

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const gulp = require('gulp'),
+  pug = require('gulp-pug'),
+  prefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  cssmin = require('gulp-cssmin'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
+  rigger = require("gulp-rigger"),
+  plumber = require('gulp-plumber'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync');
+
+const path = {
+  build: {
+    vendorsJS: 'assets/js/',
+    css: 'assets/css/'
+  },
+  src: {
+    pug: 'src/pug/pages/*.pug',
+    mainJs: 'src/assets/js/main.js',
+    vendorsJS: 'src/assets/js/vendors.js',
+    sass: 'src/assets/css/scss/main.scss'
+  },
+  watch: {
+    pug: 'src/pug/**/*.pug',
+    mainJS: [ 'src/assets/js/main.js', 'src/assets/js/main/**/*.js' ],
+    vendorsJS: [ 'src/assets/js/vendors.js', 'src/assets/js/vendors/**/*.js' ],
+    sass: 'src/assets/css/**/*.{css,scss,sass}'
+  },
+  base: './'
+};
+
+gulp.task('html:build', function () {
+  gulp.src(path.src.pug)
+    .pipe(plumber())
+    .pipe(pug({ pretty: true }))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(path.base))
+    .pipe(browserSync.stream());
+});
+gulp.task('mainJS:build', function () {
+  gulp.src(path.src.mainJs)
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(rename('index.js'))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(path.base))
+    .pipe(browserSync.stream());
+});
+gulp.task('vendorsJS:build', function () {
+  gulp.src(path.src.vendorsJS)
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(uglify())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(path.build.vendorsJS))
+    .pipe(browserSync.stream());
+});
+gulp.task('css:build', function () {
+  gulp.src(path.src.sass)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest(path.build.css))
+    .pipe(prefixer())
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(path.build.css))
+    .pipe(browserSync.reload({stream:true}));
+});
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: path.base
+    }
+  });
+});
+gulp.task('watch', function () {
+  gulp.watch(path.watch.pug, [ 'html:build' ]);
+  gulp.watch(path.watch.mainJS, [ 'mainJS:build' ]);
+  gulp.watch(path.watch.vendorsJS, [ 'vendorsJS:build' ]);
+  gulp.watch(path.watch.sass, [ 'css:build' ]);
+});
+gulp.task('build', [ 'html:build', 'mainJS:build', 'vendorsJS:build', 'css:build' ]);
+gulp.task('default', [ 'build', 'browser-sync', 'watch' ]);
---
+++ index.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const MyForm = new Form('myForm');
+
+function Form( id ) {
+  const _this = this;
+
+  this.form = document.getElementById(id);
+
+  this.form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    _this.submit();
+  });
+}
+
+Form.prototype.validate = function() {
+  let requiredFields = this.form.querySelectorAll('[data-pattern]'),
+    data = { isValid: true, errorFields: [] };
+
+  for(let field of requiredFields) {
+    const fieldPattern = field.dataset.pattern,
+      fieldName = field.getAttribute('name'),
+      fieldVal = field.value;
+
+    field.classList.remove('error');
+
+    if(!this.validateByPattern(fieldPattern, fieldVal)) {
+      data.errorFields.push(fieldName);
+      field.classList.add('error');
+    }
+  }
+
+  data.isValid = data.errorFields.length === 0;
+
+  return data;
+}
+
+Form.prototype.validateByPattern = function( pattern, value ) {
+  let isValid = true;
+
+  switch(pattern) {
+    case 'fio':
+      const fioRegExp = new RegExp('[а-яёa-z]+(?:\\-[а-яёa-z]+)?', 'gi');
+      
+      isValid = value.match(fioRegExp) !== null && value.match(fioRegExp).length === 3;
+
+      break;
+
+    case 'email':
+      const yaEmailRegExp = new RegExp('^[\\w.-]+@(?:ya\\.ru|yandex\\.(?:ru|ua|by|kz|com))$', 'i');
+      
+      isValid = yaEmailRegExp.test(value);
+
+      break;
+
+    case 'phone':
+      let sum = 0;
+      const phoneFormatRegExp = new RegExp('^\\+7\\(\\d{3}\\)\\d{3}\\-\\d{2}\\-\\d{2}$'),
+        numArray = value.match(/\d/g);
+
+      if(numArray !== null) {
+        for (let num of numArray) {
+          sum += parseInt(num);
+        }
+
+        isValid = phoneFormatRegExp.test(value) && sum <= 30;
+      } else {
+        isValid = false;
+      }
+
+      break;
+  }
+
+  return isValid;
+}
+
+Form.prototype.getData = function() {
+  let data = {};
+  const inputFields = this.form.querySelectorAll('input');
+
+  for(let field of inputFields) {
+    const fieldName = field.getAttribute('name');
+
+    data[fieldName] = field.value;
+  }
+
+  return data;
+}
+
+Form.prototype.setData = function( data ) {
+  const inputFields = this.form.querySelectorAll('input');
+
+  for(let field of inputFields) {
+    const fieldName = field.getAttribute('name');
+
+    if(fieldName in data) {
+      field.value = data[fieldName];
+    }
+  }
+}
+
+Form.prototype.submit = function() {
+  const data = this.getData(),
+    action = this.form.getAttribute('action'),
+    validate = this.validate();
+
+  if(validate.isValid) {
+    this.ajaxGet(action, data);
+  }
+}
+
+Form.prototype.ajaxGet = function( url, data ) {
+  let response;
+  const _this = this,
+    xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      _this.responseProcessing(JSON.parse(this.responseText));
+    }
+  }
+
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify(data));
+}
+
+Form.prototype.responseProcessing = function( data ) {
+  const _this = this,
+    random = Math.random();
+
+  switch(data.status) {
+    case 'progress':
+      _this.setStatus('progress');
+
+      setTimeout(function() {
+        if(random > .66) {
+          _this.ajaxGet('/handlers/progress.json' ,_this.getData());
+        } else if(random > .33) {
+          _this.ajaxGet('/handlers/success.json' ,_this.getData());
+        } else {
+          _this.ajaxGet('/handlers/error.json' ,_this.getData());
+        }
+      }, data.timeout);
+
+      break;
+    case 'success':
+      _this.setStatus('success', 'Success');
+      
+      break;
+    case 'error':
+      _this.setStatus('error', data.reason);
+      
+      break;
+  }
+}
+
+Form.prototype.setStatus = function( className = '', text = '' ) {
+  const resultContainer = this.form.querySelector('#resultContainer');
+
+  resultContainer.classList.remove('progress', 'success', 'error');
+  resultContainer.classList.add(className);
+  resultContainer.innerText = text;
+}
